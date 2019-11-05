a82b9da1e7ac4d59a589fa33fd7853c0b161826b
mishazawa <macintoshkrug@gmail.com>
2019-11-05 21:24:24 +0200

    init

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum Value {
+	EMPTY,
+	WHITE,
+	BLACK
+};
+
+public class BoardController : MonoBehaviour {
+	public float boardSize = 8;
+	public GameObject chessPrefab;
+
+	private Grid grid;
+	private Value[] chesses; 
+
+	private bool currentColor = false;
+
+	void Awake () {
+		chesses = new Value[(int)(boardSize * boardSize)];
+		grid = FindObjectOfType<Grid>();
+		transform.position += new Vector3(boardSize / 2f, 0f, boardSize / 2f);
+		Camera.main.transform.position += new Vector3(boardSize / 2f, 0f, boardSize / 2f);
+	}
+
+    void Update() {
+		HandleClick(Camera.main.ScreenPointToRay(Input.mousePosition));
+    }
+
+    void HandleClick (Ray ray) {
+		RaycastHit hit;
+    	if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out hit)) {
+    		var gridCoords = grid.Nearest(hit.point);
+    		if (IsEmpty(gridCoords)) {
+    			Value color = GetCurrentColor();
+    			GameObject chess = Instantiate(chessPrefab, gridCoords, Quaternion.identity);
+    			if (color == Value.BLACK) chess.transform.Rotate(new Vector3(180f, 0, 0));
+
+
+    			Insert(gridCoords, color);
+    			currentColor = !currentColor;
+    		}
+        }
+    }
+
+    bool IsEmpty (Vector3 coords) {
+    	int i = GetIndexByCoords(coords);
+    	return chesses[i] == Value.EMPTY;
+    }
+
+    void Insert (Vector3 coords, Value v) {
+    	chesses[GetIndexByCoords(coords)] = v;
+    }
+
+    int GetIndexByCoords (Vector3 coords) {
+    	return (int) coords.x + (int) coords.z * (int) boardSize;
+    }
+
+    Value GetCurrentColor () {
+    	return !currentColor ? Value.BLACK : Value.WHITE;
+    }
+}
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+	
+	public float size = 1f;
+	
+	public Vector3 Nearest (Vector3 position) {
+		position -= transform.position;
+		
+		Vector3 res = new Vector3(
+			(float)Mathf.RoundToInt(position.x / size) * size,
+			(float)Mathf.RoundToInt(position.y / size) * size,
+			(float)Mathf.RoundToInt(position.z / size) * size
+		);
+
+		return res + transform.position;
+	}
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.yellow;
+        for (float x = transform.position.x; x < 4; x += size)
+        {
+            for (float z = transform.position.z; z < 4; z += size)
+            {
+                var point = Nearest(new Vector3(x, 0f, z));
+                Gizmos.DrawSphere(point, 0.1f);
+            }
+                
+        }
+    }
+}
